68c978302bb4fbbc2d74a21ed0609cf29e4a4514
reasonsolo <reasonsolo@gmail.com>
2012-10-13 22:46:15 +0800

    search result with title

--- suibian/suibian/search.py
+++ suibian/suibian/search.py
@@ -2,14 +2,11 @@ from django.conf import settings
 import happybase
 import jieba
 
-def get_table(tablename):
+
+def search(kwords_lst):
     conn = happybase.Connection(host = settings.HBASE_HOST,
                                 port = settings.HBASE_PORT,
                                 compat = '0.90')
-    return conn.table(tablename)
-
-
-def search(kwords_lst):
     ksegs = []
     for kwords in kwords_lst:
         segs = jieba.cut(kwords, cut_all = True)
@@ -20,7 +17,7 @@ def search(kwords_lst):
         ksegs += unicode_segs
     ksegs = set(ksegs)
 
-    index_table = get_table('IndexResult')
+    index_table = conn.table('IndexResult')
     hbase_records = {}
     search_results = []
     rows = index_table.rows(ksegs)
@@ -40,7 +37,14 @@ def search(kwords_lst):
        hbase_records[key] = record
 
     # TODO: better search logic
-    return set(search_results)
+    result_urls = set(search_results)
+    url_table = conn.table('WebData')
+    rows = url_table.rows(result_urls)
+    results = []
+    for url, data in rows:
+        title = data['content:title']
+        results.append([url, title])
+    return results
 
 
 
